1d373bc7540fd258309b2a92fde680d7534dc49f
Auguste Poiroux <augustepoiroux@gmail.com>
2025-05-08 16:50:40 +0200

    Add backtracking to chc_auto

--- src/ce_api.ml
+++ src/ce_api.ml
@@ -19,6 +19,7 @@ open Elim
 open Equality
 open Tactypes
 open Proofview.Notations
+open Auto
 
 module PV = Proofview
 
@@ -114,27 +115,58 @@ let do_nothing i : unit PV.tactic =
     Tacticals.tclIDTAC
   end
 
-(* Tactic: oltauto with certification*)
+(* Wrapper for the trivial tactic *)
+let trivial_tac () =
+  Auto.gen_auto ~debug:Off None [] None
 
-(* Simple proof search for inductive goals *)
-let rec solve_inductive_goal gl =
-  let env = Proofview.Goal.env gl in
-  let sigma = Proofview.Goal.sigma gl in
-  let concl = Proofview.Goal.concl gl in
-  match try Some (pf_apply Tacred.reduce_to_atomic_ind gl concl) with _ -> None with
-  | Some ((ind, u), t) ->
-      let IndType (indf, _realargs) = find_rectype env sigma t in
-      let constrs = Inductiveops.get_constructors env indf in
-      let rec try_constructors = function
-        | [] -> Tacticals.tclZEROMSG (Pp.str "No constructor applies.")
-        | cstr :: rest ->
-            let c = EConstr.mkConstructU cstr.cs_cstr in
-            Tacticals.tclORELSE
-              (Tacticals.tclTHEN (Tactics.apply c) (Tacticals.tclREPEAT (Proofview.Goal.enter solve_inductive_goal)))
-              (try_constructors rest)
-      in
-      try_constructors (Array.to_list constrs)
-  | None -> Tacticals.tclIDTAC
+let rec solve_inductive_goal max_depth depth gl =
+  if depth > max_depth then
+    Tacticals.tclFAIL ~info:Exninfo.null (Pp.str "Exceeded maximum search depth")
+  else
+    let env = Proofview.Goal.env gl in
+    let sigma = Proofview.Goal.sigma gl in
+    let concl = Proofview.Goal.concl gl in
+    match try Some (pf_apply Tacred.reduce_to_atomic_ind gl concl) with _ -> None with
+    | Some ((ind, u), t) ->
+        let IndType (indf, _realargs) = find_rectype env sigma t in
+        let constrs = Inductiveops.get_constructors env indf in
 
-(* Export the tactic for use in a grammar extension *)
-let chc_auto = Proofview.Goal.enter solve_inductive_goal
+        (* Try each constructor with backtracking *)
+        let rec try_constructors = function
+          | [] -> Tacticals.tclFAIL ~info:Exninfo.null (Pp.str "No constructor applies")
+          | cstr :: rest ->
+              (* Apply constructor then try to solve subgoals recursively *)
+              let try_this_constructor =
+                let c = EConstr.mkConstructU cstr.cs_cstr in
+                Tacticals.tclTHEN
+                  (Tactics.apply c)
+                  (Proofview.Goal.enter (solve_subgoals max_depth (depth + 1)))
+              in
+              (* If this constructor doesn't work, try the next one *)
+              Tacticals.tclORELSE try_this_constructor (try_constructors rest)
+        in
+        try_constructors (Array.to_list constrs)
+
+    | None ->
+        (* For non-inductive goals, try tactics that might solve them directly *)
+        Tacticals.tclFIRST [
+          Tactics.reflexivity;
+          Tactics.assumption;
+          trivial_tac ();
+          Tacticals.tclFAIL ~info:Exninfo.null (Pp.str "Not an inductive goal and no direct tactic applies")
+        ]
+
+(* Try to solve all subgoals recursively with backtracking *)
+and solve_subgoals max_depth depth gl =
+  Tacticals.tclPROGRESS (
+    Tacticals.tclFIRST [
+      solve_inductive_goal max_depth depth gl;
+      Tactics.reflexivity;
+      Tactics.assumption;
+      trivial_tac ();
+    ]
+  )
+let chc_auto ?(max_depth=20) () =
+  Tacticals.tclCOMPLETE (
+    Proofview.Goal.enter (solve_inductive_goal max_depth 0)
+  )
--- theories/CoLog.v
+++ theories/CoLog.v
@@ -7,14 +7,45 @@ Inductive PP: nat -> Prop :=
 Inductive QQ: Prop :=
   | Q0 : QQ.
 
-(* Goal PP 5.
-  Show Proof.
-  dno 5.
-  Show Proof.
-Abort. *)
-
+(* Test simple inductive goal *)
 Goal PP 5.
   Show Proof.
   chc_auto.
   Show Proof.
+Qed.
+
+(* Test with another simple inductive type *)
+Inductive Even : nat -> Prop :=
+  | Even0 : Even 0
+  | EvenS n : Odd n -> Even (S n)
+with Odd : nat -> Prop :=
+  | OddS n : Even n -> Odd (S n).
+
+Goal Even 4.
+  Show Proof.
+  chc_auto with depth 4.
+  Show Proof.
+Qed.
+
+Inductive PPn: nat -> Prop :=
+  | Pn0 : PPn 0
+  | Pn2 {n} : PPn n -> PPn (S (S n))
+  | Pn3 {n} : PPn n -> PPn (S (S (S n))).
+
+Goal PPn 5.
+  Show Proof.
+  (* repeat constructor; fail. *)
+  chc_auto.
+  Show Proof.
+Qed.
+
+Goal PPn 1.
+  Show Proof.
+  try chc_auto.
 Abort.
+
+Goal PPn 5.
+  Show Proof.
+  chc_auto with depth 2.
+  Show Proof.
+Qed.
