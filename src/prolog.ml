8dbec1c654e4f4fab5a5e92db12dbc383095e125
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2025-05-19 09:23:04 +0200

    Rewrite proof search to produce a proof tree

--- src/prolog.ml
+++ src/prolog.ml
@@ -5,14 +5,6 @@
 
 open Coutils
 
-(*
-  TODO: REWRITE WRT CHANGED CLAUSES
-
-  Every clause should carry an integer ID, and we cannot just reduce clauses
-  dynamically without care. We need to be sure that the ID is preserved,
-  alongside the instantiations that led to the reduction.
-*)
-
 (* Formula definitions *)
 
 (* term algebra T(F, X) *)
@@ -58,8 +50,8 @@ let is_fact = function
   | Clause (_, _, Atom _, []) -> true
   | _ -> false
 
-let clause_of f = Clause (0, 0, f, [])
-
+let dest_hd (Clause (_, _, f, _)) = f
+  
 (* fact database *)
 type db = clause list
 
@@ -70,6 +62,19 @@ type subst = (var * term) list
 type iclause =
   | IClause of subst * clause
 
+type proof_tree =
+  | Leaf of iclause 
+  (* Leaf of fact *)
+  (* can only be produced as the instantiation of a fact clause in the problem *)
+  (* the problem is unsolvable if it has no fact clauses, as branches must stay open *)
+  | Node of iclause * proof_tree list
+  (* Node of reduced_clause * arguments_for_body *) 
+  (* invariant :: reduced_clause has empty body (is a proven atomic fact if its branches are closed) *)
+  (* note: some variables may not be fully instantiated if the proof is constructor bottom up *)
+  (* this corresponds to proofs of universally quantified atoms *)
+  | Open of formula 
+  (* Open of goal *)
+  
 let empty_subst = []
 
 (* Check if `a` maps to `b` under `sub` *)
@@ -124,7 +129,7 @@ let matches (f1: formula) (f2: formula) : subst option =
       List.fold_left2 term_matches (Some empty_subst) ts ss
   | _ -> None
 
-let match_and_resolve (clause: clause) (goal: formula): formula list option =
+let match_and_resolve (clause: clause) (goal: formula): (subst * formula list) option =
   match clause with
   | Clause (id, nv, head, body) ->
     let sub_opt = matches head goal in
@@ -133,76 +138,92 @@ let match_and_resolve (clause: clause) (goal: formula): formula list option =
     | Some s ->
       (* unified body atoms as new goals *)
       let body' = body |> List.map (apply_subst_formula s) in
-      Some body'
+      Some (s, body')
 
-(* dfs *)
-let dfs (clauses: clause_set) (goal: formula) : clause list option =
-  (* prevent loops *)
-  let visited_tbl = Hashtbl.create 32 in
-  let visited_or_visit g = 
-    if Hashtbl.mem visited_tbl g then
-      true
-    else begin
-      Hashtbl.add visited_tbl g ();
-      false
-    end
+let dfs (clauses: clause_set) (goal: formula) : proof_tree option =
+  (* memoize *)
+  let proof_cache = Hashtbl.create 32 in
+  (* helpers *)
+  let facts = List.filter is_fact clauses in
+  (* actual dfs *)
+  (*
+    invariant: since we move top-down, 
+    the goal argument is always fully instantiated
+  *)
+  let rec dfs_step visited (goal: formula) : proof_tree option =
+    if Hashtbl.mem proof_cache goal then
+      (* already proved or disproved *)
+      Hashtbl.find proof_cache goal
+    else if List.mem goal visited then
+      (* this is a non-terminating branch *)
+      (* backtrack *)
+      (* it is safe (and recommended) to check the cache and visited in this order *)
+      (* a proof for this goal may have been discovered in another otherwise failed branch *)
+      None
+    else 
+      (* attempt to close early with a fact *)
+      let fact_opt = facts 
+                      |> List.find_map (function
+                        | Clause (idx, nv, hd, _) -> 
+                          matches hd goal 
+                            |> Option.map (fun s -> 
+                              IClause (s, Clause (idx, nv, goal, []))
+                            )
+                      ) 
+      in
+      match fact_opt with
+      | Some f -> 
+        (* found a fact, return it *)
+        let proof = Some (Leaf f) in
+        Hashtbl.add proof_cache goal proof;
+        proof
+      | None ->
+        (* attempt to branch and resolve *)
+        let visited' = goal :: visited in 
+        clauses
+          |> List.find_map (fun c ->
+            (* 
+              first clause that not only matches, 
+              but has resolvable branches too
+            *) 
+            match_and_resolve c goal
+            (* can we resolve each of the branches? *)
+            |> Option.map ( fun (s, bs) ->
+              let inner = 
+                bs 
+                  |> List.rev
+                  |> List.fold_left (fun (failed, acc) b ->
+                    if failed then 
+                      (* don't try the remaining *)
+                      (failed, acc)
+                    else
+                      (* keep going *)
+                      match dfs_step visited' b with
+                      | None -> (true, acc)
+                      | Some proof -> 
+                        (false, proof :: acc)                    
+                  ) (false, [])
+              in
+              match inner with
+              | (true, _) -> None
+              | (false, proofs) -> Some (c, s, proofs)
+            )
+            |> Option.flatten
+          )
+          |> Option.map (fun (c, s, proofs) ->
+              (* all branches were closed, wrap it in a node *)
+              let Clause (id, nv, _, _) = c in
+              let top = Clause (id, nv, goal, []) in
+              let proof = Node (IClause (s, top), proofs) in
+              (* add to the cache *)
+              Hashtbl.add proof_cache goal (Some proof);
+              proof
+            )
   in
-  (* run dfs with backtracking to reduce a list of goals to a fact db *)
-  (* returns the list of clauses resolved against (in reverse order) *)
-  let rec dfs_step cs db goals acc =
-    match goals with
-    | [] -> Some acc (* no more goals, steps complete *)
-    | g :: gs -> 
-      if visited_or_visit g then 
-        (* this is a non-terminating branch *)
-        (* backtrack *)
-        None
-      else if List.mem g db then
-        (* this is a fact, discharge goal *)
-        dfs_step cs db gs (clause_of g :: acc)
-      else
-        (* resolve against clauses that match, exploring each branch while
-        backtracking *)
-        (* resolve this goal *)
-        (* intentionally reuses the full clause set *)
-        let res = explore cs db g gs acc in
-        match res with
-        | None -> 
-          (* cannot prove this goal, fail *)
-          None
-        | Some path ->
-          (* found a path, continue with other goals *)
-          dfs_step cs db gs (path @ acc)
-  (* explore the branches that reduce a given goal, backtracking and resuming
-  dfs as necessary*)
-  and explore cs db g gs acc =
-    match cs with
-    | [] -> None
-    | c :: cs' ->
-      match match_and_resolve c g with
-      | None -> 
-        (* no match, continue exploring *)
-        explore cs' db g gs acc
-      | Some new_goals ->
-        (* add the new goals to the list of goals, restart dfs *)
-        let branch = dfs_step cs db (new_goals @ gs) (c :: acc) in
-        match branch with
-        | Some _ -> branch
-        | None -> 
-          (* backtrack, continue exploring *)
-          explore cs' db g gs acc
-  in
-  let opt_dest_fact = function 
-    | Clause (_, _, f, []) -> Some f 
-    | _ -> None
-  in
-  let db = List.filter_map opt_dest_fact clauses in
-  let cs = List.filter (not << is_fact) clauses in 
-  let dfs_res = dfs_step cs db [goal] [] in
-    Option.map List.rev dfs_res
+  dfs_step [] goal
 
 (* API *)
 
 (* given a set of clauses and a goal, produce a list of steps that can be sequentially applied  *)
-let query (clauses: clause_set) (goal: formula) : clause list option =
+let query (clauses: clause_set) (goal: formula) : proof_tree option =
   dfs clauses goal
